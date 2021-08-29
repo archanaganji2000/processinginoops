7c5b7e91b2e9af9d40e9792d99a5b50ff7c0a610
archana <ganjiarchana2000@gmail.com>
2021-08-29 19:50:59 +0530

    add moving ball code

---
+++ src/main/java/Main.java
@@ -0,0 +1,57 @@
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+    public static final int HEIGHT = 480;
+    public static final int WIDTH = 640;
+    public static void main(String[] args) {
+        PApplet.main("Main", args);
+    }
+
+    @Override
+    public void settings() {
+        super.settings();
+        size(WIDTH, HEIGHT);
+    }
+
+    ball[] b = new ball[4];
+
+    @Override
+    public void setup() {
+        int k=1;
+        for (int i = 0; i < b.length; i++) {
+            b[i] = new ball(0,HEIGHT*k/5,i+1);
+            k++;
+        }
+
+    }
+
+    @Override
+    public void draw() {
+        for (ball i : b) {
+            i.display();
+
+        }
+
+    }
+    public class ball {
+        float x , y , velx;
+        ball(float x,float y,float velx)
+        {
+            this.x=x;
+            this.y=y;
+            this.velx=velx;
+        }
+
+        void display() {
+
+            ellipse(x,y, 10, 10);
+            x = x + velx;
+        }
+
+
+
+    }
+}
+
+
