e22c84d19e8f6116fbc890a5363fcf1c2b4e64cd
archana <ganjiarchana2000@gmail.com>
2021-08-29 20:02:50 +0530

    add java file

--- src/main/java/processinginoops.java
+++ src/main/java/processinginoops.java
@@ -1,11 +1,11 @@
 import processing.core.PApplet;
 
-public class Main extends PApplet {
+public class processinginoops extends PApplet {
 
     public static final int HEIGHT = 480;
     public static final int WIDTH = 640;
     public static void main(String[] args) {
-        PApplet.main("Main", args);
+        PApplet.main("processinginoops", args);
     }
 
     @Override
